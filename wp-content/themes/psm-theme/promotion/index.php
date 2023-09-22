d4aeee5e003a371ec5051fc49ac051a2b355a53f
vargabyan <vardan.gabrielyan.it@gmail.com>
2023-09-22 11:36:28 +0500

    #6

---
+++ wp-content/themes/psm-theme/promotion/index.php
@@ -0,0 +1,261 @@
+<link rel="stylesheet" href="/wp-content/themes/psm-theme/css/fonts.css">
+<link rel="stylesheet" href="/wp-content/themes/psm-theme/promotion/promotion.css">
+<link rel="stylesheet" href="/wp-content/themes/psm-theme/style.css">
+
+<div class="container-width1110">
+
+    <div class="promotion-page">
+        <p class="promotion-page_header">Акции</p>
+
+        <p class="promotion-page_description">
+            На этой странице вы сможете ознакомиться с текущими акциями и предложениями по аренде автовышек от компании
+            <br> ООО «Предприятие строительных машин». Обращаем ваше внимание — срок действия предложений ограничен.
+        </p>
+
+        <div class="promotion-page_offer-wrapper">
+            <p class="promotion-page_offer">
+                <span>Предложение от 29-01-2020 г.</span>
+                При аренде автовышки от 14 дней перебазировка в любой регион России бесплатно! Подробности по телефону:
+            </p>
+            <a class="promotion-page_tel" href="tel:+79022627066">+7 (902) 262-70-66</a>
+            <a class="promotion-page_leave-review" href="">Оставить заявку</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="work-steps-block width-block">
+    <div class="header-box">
+        <p>Как мы <span>работаем</span></p>
+    </div>
+    <div class="work-steps-box">
+        <div>
+            <p class="part-1">/01</p>
+            <p class="part-2">Ваш звонок</p>
+            <p class="part-3">Рыбныыыыыыый текст который покорит сердца ваших клиентов</p>
+        </div>
+        <div>
+            <p class="part-1">/02</p>
+            <p class="part-2">ответ диспетчера</p>
+            <p class="part-3">Рыбныыыыыыый текст который покорит сердца ваших клиентов</p>
+        </div>
+        <div>
+            <p class="part-1">/03</p>
+            <p class="part-2">подписываем договор</p>
+            <p class="part-3">Рыбныыыыыыый текст который покорит сердца ваших клиентов</p>
+        </div>
+        <div>
+            <p class="part-1">/04</p>
+            <p class="part-2">отличный результат!</p>
+            <p class="part-3">Рыбныыыыыыый текст который покорит сердца ваших клиентов</p>
+        </div>
+    </div>
+    <div class="button-box">
+        <button>Смотреть каталог автовышек</button>
+    </div>
+</div>
+
+
+<div class="product-showcase width-block">
+    <h2>Наши машины</h2>
+
+    <div class="category-box">
+        <div class="category-swiper-button-prev">
+            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 9L1 5L5 1" stroke="white"/>
+            </svg>
+        </div>
+
+        <div class="category-swiper">
+            <div class="swiper-wrapper category-swiper-wrapper">
+
+                <!--                --><?php //= do_shortcode('[cat]'); ?>
+
+            </div>
+        </div>
+
+        <div class="category-swiper-button-next">
+            <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 1L5 5L1 9" stroke="white"/>
+            </svg>
+        </div>
+    </div>
+
+    <div class="products-box">
+        <!--        --><?php //= do_shortcode('[auto]'); ?>
+    </div>
+</div>
+
+
+<div class="container-width1110">
+
+    <a class="about-company_button-go-to-category-elevator" href="">Перейти в каталог автовышек</a>
+
+</div>
+
+
+<div class="help-block width-block">
+    <div class="left-box">
+        <div class="part-1">
+            <p>Поможем подобрать оборудование</p>
+        </div>
+        <div class="part-2">
+            <div>
+                <img src="/wp-content/uploads/2023/07/Ellipse%2086.png" alt=""/>
+            </div>
+            <p>Ответим на все ваши вопросы и поможем выбрать оборудование</p>
+        </div>
+        <div class="part-3">
+            <p>Не любите говорить по телефону? Пишите в месенджеры</p>
+        </div>
+        <div class="part-4">
+            <div>
+                <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.3 26.7C7.5 26.2 3.6 29.1 3 28.4C2.4 27.8 5 23.7 4.5 23C2.9 20.8 2 18.1 2 15.2C2 7.80001 8 1.70001 15.5 1.70001C23 1.70001 29 7.80001 29 15.2C29 22.6 23 28.7 15.5 28.7C14.5 28.7 13.6 28.6 12.7 28.4"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                    <path d="M12.7 8.40003L13.9 11.4C14 11.6 13.9 11.8 13.7 11.9C13.2 12.2 12.5 12.9 13.3 14C14.3 15.4 17 18.9 19.2 16.6C19.3 16.5 19.5 16.4 19.7 16.5L22.9 17.9C23.1 18 23.2 18.3 23.1 18.5C22.4 19.9 19.7 23.6 13 17.8C6.20003 11.9 10.6 9.00004 12.1 8.30004C12.3 8.00004 12.6 8.10003 12.7 8.40003Z"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <div class="telegram-icon-box">
+                <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.10093 14.6C8.90093 12 28.5009 4 28.5009 4L24.6009 26.6C24.4009 27.7 23.1009 28.1 22.3009 27.4L16.2009 22.3L11.9009 26.3L12.6009 19.6L25.6009 7.3L9.60093 17.3L10.6009 23L7.30093 17.7L2.30093 16.1C1.50093 15.8 1.40093 14.8 2.10093 14.6Z"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+    </div>
+    <div class="line"></div>
+    <div class="right-box">
+        <label>
+            Имя
+            <input type="text" placeholder="Иван">
+        </label>
+        <label class="number-label">
+            Номер телефона
+            <input type="text" placeholder="8 900 000 00 00">
+        </label>
+        <button>Заказать дом мечты</button>
+        <p>Я даю свое согласие на обработку персональных данных</p>
+    </div>
+    <div class="hidden-part">
+        <p>Не любите говорить по телефону? Пишите в месенджеры</p>
+        <div class="icon-box">
+            <div>
+                <img src="/wp-content/uploads/2023/07/Ellipse%2086.png" alt=""/>
+            </div>
+            <div>
+                <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.3 26.7C7.5 26.2 3.6 29.1 3 28.4C2.4 27.8 5 23.7 4.5 23C2.9 20.8 2 18.1 2 15.2C2 7.80001 8 1.70001 15.5 1.70001C23 1.70001 29 7.80001 29 15.2C29 22.6 23 28.7 15.5 28.7C14.5 28.7 13.6 28.6 12.7 28.4"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                    <path d="M12.7 8.40003L13.9 11.4C14 11.6 13.9 11.8 13.7 11.9C13.2 12.2 12.5 12.9 13.3 14C14.3 15.4 17 18.9 19.2 16.6C19.3 16.5 19.5 16.4 19.7 16.5L22.9 17.9C23.1 18 23.2 18.3 23.1 18.5C22.4 19.9 19.7 23.6 13 17.8C6.20003 11.9 10.6 9.00004 12.1 8.30004C12.3 8.00004 12.6 8.10003 12.7 8.40003Z"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <div class="telegram-icon-box">
+                <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.10093 14.6C8.90093 12 28.5009 4 28.5009 4L24.6009 26.6C24.4009 27.7 23.1009 28.1 22.3009 27.4L16.2009 22.3L11.9009 26.3L12.6009 19.6L25.6009 7.3L9.60093 17.3L10.6009 23L7.30093 17.7L2.30093 16.1C1.50093 15.8 1.40093 14.8 2.10093 14.6Z"
+                          stroke="#414042" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round"
+                          stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="/wp-content/themes/psm-theme/js/home.js"></script>
+
+
+
+<!--old code-->
+<section class="main">
+    <div class="container">
+        <div class="row">
+            <h1 style="text-align: center;">Акции</h1>
+            <p style="text-align: left;">На этой странице вы сможете ознакомиться с текущими акциями и предложениями по аренде автовышек от компании ООО "Предприятие строительных машин". Обращаем ваше внимание - срок действия предложений ограничен.</p>
+            &nbsp;
+            <div class="col-md-4">
+                <h4><em>Предложение от 29-01-2020 г.</em></h4>
+                <h2>При аренде автовышки от 14 дней перебазировка в любой регион России бесплатно! Подробности по телефону: <a class="phone" href="tel:+7 (902) 262-70-66">+7 (902) 262-70-66</a></h2>
+                <ul>
+                    <li style="list-style-type: none;"></li>
+                </ul>
+                &nbsp;
+
+            </div>
+            <div class="col-md-8 adaptivecol-img"><img class="alignleft wp-image-880" src="/wp-content/uploads/2019/12/arenda-avtovyshki-akcii.png" alt="Акции" width="600" height="151" /></div>
+            <div class="col-md-4">
+                <h2></h2>
+                <ul>
+                    <li style="list-style-type: none;"></li>
+                </ul>
+                &nbsp;
+
+            </div>
+            <div class="col-md-8 adaptivecol-img"></div>
+        </div>
+        <div class="col-xs-12 order-form"><form>
+                <div class="row">
+                    <div id="recaptcha6" class="g-recaptcha" data-size="invisible"></div>
+                    <div class="col-md-4"><input name="name" type="text" placeholder="Ваше имя" /></div>
+                    <div class="col-md-4"><input name="tel" type="text" placeholder="+7(999)999-99-99" /></div>
+                    <div class="col-md-4"><input name="addr" type="text" placeholder="Ваш e-mail" /></div>
+                    <div class="col-md-12"><input name="descr" type="text" placeholder="Подробнее о заказе" />
+                        <input name="policy" type="checkbox" />Я принимаю <a href="/policy">соглашение сайта об обработке персональных данных.</a>
+                        <button class="order-but but-auto" type="submit">Заказать</button>
+                        <div class="output-form"></div>
+                    </div>
+                </div>
+            </form></div>
+    </div>
+</section>
+<div class="stage-section">
+    <h2 class="text-center">Этапы работ</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="stage-card"><img class="alignnone" src="/wp-content/uploads/2019/10/arenda-avtovyshki-foto1.png" alt="Аренда автовышки" width="188" height="116" /></div>
+                <h3 class="text-center">Ваш звонок</h3>
+            </div>
+            <div class="col-sm-3">
+                <div class="stage-card"><img class="alignnone" src="/wp-content/uploads/2019/10/arenda-avtovyshki-foto2.png" alt="Аренда автовышки" width="188" height="116" /></div>
+                <h3 class="text-center">Ответ диспетчера</h3>
+            </div>
+            <div class="col-sm-3">
+                <div class="stage-card"><img class="alignnone" src="/wp-content/uploads/2019/10/arenda-avtovyshki-foto13-1.png" alt="Аренда автовышки" width="188" height="116" /></div>
+                <h3 class="text-center">Подписываем договор</h3>
+            </div>
+            <div class="col-sm-3">
+                <div class="last-stage-card"><img class="alignnone" src="/wp-content/uploads/2019/10/arenda-avtovyshki-foto4.png" alt="Аренда автовышки" width="188" height="116" /></div>
+                <h3 class="text-center">Отличный результат!</h3>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="product-section">
+    <h2 class="text-center">Наши машины</h2>
+    <div class="container">
+        <div class="row">
+
+            [auto]
+
+        </div>
+    </div>
+</div>
+<div class="category-section">
+    <div class="container">
+        <div class="row all-hidden catrow all-hidden all-cat">
+
+            [cat]
+
+        </div>
+        <div class="flexdiv"><a class="show-all">Показать все</a></div>
+        <div class="flexdiv"><a class="hide-all" style="display: none;">Скрыть все</a></div>
+    </div>
+</div>
